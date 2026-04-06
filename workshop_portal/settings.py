a73c9b763859cd73048193159f2ffbff4f95376f
AYUSH SHARMA <ayushsharma6263464@gmail.com>
2026-04-06 12:40:00 +0530

    Upgrade Django from 3.0.7 to 4.2.16 for Python 3.12 compatibility and add DEFAULT_AUTO_FIELD setting

--- workshop_portal/settings.py
+++ workshop_portal/settings.py
@@ -148,6 +148,10 @@ EMAIL_HOST_USER = EMAIL_HOST_USER
 EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
 EMAIL_PORT = EMAIL_PORT
 EMAIL_USE_TLS = EMAIL_USE_TLS
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_TIMEOUT = 300
 SENDER_EMAIL = SENDER_EMAIL
 
